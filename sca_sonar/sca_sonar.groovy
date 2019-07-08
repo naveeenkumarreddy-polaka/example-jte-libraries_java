8540f1f8d200131dd72323330da866c7875990d1
naveeenkumarreddy-polaka <40000751+naveeenkumarreddy-polaka@users.noreply.github.com>
2019-07-08 17:24:34 +0530

    Update sca_sonar.groovy

--- sca_sonar/sca_sonar.groovy
+++ sca_sonar/sca_sonar.groovy
@@ -2,9 +2,7 @@ void call(){
 echo "Starting Static Code Analysis"       
 stage('SCA_Sonar'){
        node{
-          bat '"C:\\ProgramData\\chocolatey\\bin\\SonarScanner.MSBuild.exe" begin /k:DancingGoat'
-        	bat '"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" DancingGoat.sln /t:Rebuild'
-        	bat '"C:\\ProgramData\\chocolatey\\bin\\SonarScanner.MSBuild.exe" end'
+          bat "mvn packge sonar:sonar"
        }
 } 
 echo "Ending Static Code Analysis"
