81374a6a0a1c6a64d0879aba204eb6aad6d54010
naveeenkumarreddy-polaka <40000751+naveeenkumarreddy-polaka@users.noreply.github.com>
2019-07-08 17:25:47 +0530

    Update unit_test.groovy

--- unit_test/unit_test.groovy
+++ unit_test/unit_test.groovy
@@ -3,9 +3,7 @@ echo "Starting Unit Test"
 stage("Unit_Test"){
        node{
 	  echo 'Unit testing is in progress'     
-	  sleep(10)
-          //bat returnStatus: true, script: "\dotnet.exe\" test \"${workspace}/DancingGoat.sln\" --logger \"trx;LogFileName=unit_tests.xml\" --no-build"
-	  //step([$class: 'MSTestPublisher', testResultsFile:"**/*.trx", failOnError: true, keepLongStdio: true])*/
+	  sleep(5)
        }       
 } 
 echo "Ending Unit Test"
