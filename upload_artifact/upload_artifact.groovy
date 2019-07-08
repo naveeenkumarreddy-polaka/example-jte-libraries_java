4bd2e5f1845e61250d24a41310ce01ff8e69ca1a
naveeenkumarreddy-polaka <40000751+naveeenkumarreddy-polaka@users.noreply.github.com>
2019-07-08 17:27:28 +0530

    Update upload_artifact.groovy

--- upload_artifact/upload_artifact.groovy
+++ upload_artifact/upload_artifact.groovy
@@ -5,7 +5,9 @@ stage("Upload_Artifact"){
     node{
         sleep(5)
         /*withAWS(region:'us-east-2',credentials:'<Credentials_Id>') {
-        s3Upload(file:"C:\\users\\venkataramana.t\\.jenkins\\workspace\\dotnet_project\\DemoApp\\obj\\release\\package\\DemoApp-${env.BUILD_NUMBER}.zip", bucket:'<bucket-name>')
+        s3Upload(file:"C:\\users\\venkataramana.t\\.jenkins\\workspace\\*.${env.BUILD_NUMBER}.zip", bucket:'<bucket-name>')
+        (OR)
+       bat "mvn clean deploy"
         }*/
     }
 }
