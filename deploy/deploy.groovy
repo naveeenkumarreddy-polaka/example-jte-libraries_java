39ec95f4ef628fc952ce6c7576e5a3dd9fd37811
naveeenkumarreddy-polaka <40000751+naveeenkumarreddy-polaka@users.noreply.github.com>
2019-07-08 17:20:55 +0530

    Update deploy.groovy

--- deploy/deploy.groovy
+++ deploy/deploy.groovy
@@ -3,8 +3,8 @@ void call(){
 echo "Starting Deployment"
 stage("Deploy"){
     node{
-        sleep(5)
-	//"c:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:package=dancing.zip -dest:auto,computerName=localhost    
+        sleep(1)
+	//deploy to tomcat    
     }
 }
 echo "Auto deployment done"	
