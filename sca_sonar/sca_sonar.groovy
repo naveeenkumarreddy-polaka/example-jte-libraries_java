c71128255d51df6a63ec8b86e5fc3e686932e03f
naveeenkumarreddy-polaka <40000751+naveeenkumarreddy-polaka@users.noreply.github.com>
2019-07-10 18:21:45 +0530

    Update sca_sonar.groovy

--- sca_sonar/sca_sonar.groovy
+++ sca_sonar/sca_sonar.groovy
@@ -2,7 +2,7 @@ void call(){
 echo "Starting Static Code Analysis"       
 stage('SCA_Sonar'){
        node{
-          //bat "mvn package sonar:sonar"
+          bat "mvn package sonar:sonar"
        }
 } 
 echo "Ending Static Code Analysis"
