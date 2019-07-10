ce707e783f9531f9b68e87860690d33430fd39a8
naveeenkumarreddy-polaka <40000751+naveeenkumarreddy-polaka@users.noreply.github.com>
2019-07-10 17:52:26 +0530

    Update build.groovy

--- build/build.groovy
+++ build/build.groovy
@@ -3,9 +3,9 @@ echo "starting msbuild"
 stage('build') {
        echo "build the code"
        node {
-              extensions: [[$class: 'CloneOption', timeout: 120]],
+              extensions: [[$class: 'CloneOption', timeout: 120]]
               bat "mvn package"
        }
   } 
-echo "ending msbuild"
+echo "ending build"
 }
